9ec3bc56545dcd82e6a461b2c69331763d29b5cb
AbdAlghani Alkouwatli <aboodekwt2001@gmail.com>
2023-06-12 21:05:21 +0300

    app: adding swagger to the app:

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { NestFactory } from "@nestjs/core";
+import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Makhzan")
+    .setDescription("makhzan api docs")
+    .setVersion("1.0")
+    .addTag("makhzan")
+    .build();
+  const options = {
+    deepScanRoutes: true,
+  };
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup("api", app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/warehouse/dtos/create-warehouse.dto.ts
+++ src/warehouse/dtos/create-warehouse.dto.ts
@@ -1,13 +1,18 @@
-import { IsString, IsPhoneNumber, IsOptional } from 'class-validator';
+import { ApiProperty } from "@nestjs/swagger";
+import { Expose } from "class-transformer";
+import { IsString, IsPhoneNumber, IsOptional } from "class-validator";
 
 export class CreateWarehouseDto {
+  @ApiProperty()
   @IsString()
   name: string;
 
+  @ApiProperty()
   @IsOptional()
   @IsString()
   location: string;
 
+  @ApiProperty()
   @IsPhoneNumber()
   phoneNumber: string;
 }

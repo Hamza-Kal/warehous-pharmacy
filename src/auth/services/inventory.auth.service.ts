39d270b4bd54883bfb0b56465a6514308f51c382
AbdAlghani Alkouwatli <aboodekwt2001@gmail.com>
2023-07-16 22:23:13 +0300

    app: testing committing to softdev

--- src/auth/services/inventory.auth.service.ts
+++ src/auth/services/inventory.auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UserService } from 'src/user/services/user.service';
 
 @Injectable()
-export class InventoryAuthService {}
+export class InventoryAuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
+}

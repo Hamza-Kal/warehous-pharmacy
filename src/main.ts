633dbbc58a65ee8d00bf8a85658935c631fbe1f0
AbdAlghani Alkouwatli <aboodekwt2001@gmail.com>
2023-07-26 11:49:45 +0300

    .

--- src/main.ts
+++ src/main.ts
@@ -17,8 +17,8 @@ async function bootstrap() {
   };
   const document = SwaggerModule.createDocument(app, config, options);
   SwaggerModule.setup('api', app, document);
-  await app.listen(3000, () => {
-    console.log('Running Server on Localhost 3000');
+  await app.listen(parseInt(process.env.PORT) || 3000, () => {
+    console.log(`running on port ${process.env.PORT}`);
   });
 }
 bootstrap();

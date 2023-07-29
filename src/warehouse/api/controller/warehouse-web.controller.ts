3e9bce3a3d231f476ddcf845794e4b09e037658c
Faek Alayoubi <falayoubi@darsoft.net>
2023-07-29 22:38:35 +0300

    ..

--- src/warehouse/api/controller/warehouse-web.controller.ts
+++ src/warehouse/api/controller/warehouse-web.controller.ts
@@ -37,7 +37,10 @@ export class WarehouseController {
     role: [Role.GUEST],
     completedAccount: false,
   })
-  async completeInfo(@Body() body: CreateWarehouseDto, @CurrUser() user: IUser) {
+  async completeInfo(
+    @Body() body: CreateWarehouseDto,
+    @CurrUser() user: IUser,
+  ) {
     return this.warehouseWebService.createWarehouse(body, user);
   }
 

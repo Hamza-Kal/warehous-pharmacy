fb0a8fbcf5a249e35cc758cbafb23b6278901c2f
AbdAlghani Alkouwatli <aboodekwt2001@gmail.com>
2023-08-08 23:21:28 +0300

    .

--- src/shared/Types/errorsCode.ts
+++ src/shared/Types/errorsCode.ts
@@ -8,6 +8,6 @@ export const errorsCode = {
   notFoundInventory: 90000,
   notFoundReturnOrder: 100000,
   notFoundReportMedicine: 110000,
-  notFoundPharmacy: 110000,
+  notFoundPharmacy: 120000,
   notFoundWarehouse: 120000,
 };

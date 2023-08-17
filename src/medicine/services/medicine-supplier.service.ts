eb24e7484e98ba6754f2283304addb012fa9ae49
AbdAlghani Alkouwatli <aboodekwt2001@gmail.com>
2023-08-17 11:33:33 +0300

    fixing exception status

--- src/medicine/services/medicine-supplier.service.ts
+++ src/medicine/services/medicine-supplier.service.ts
@@ -225,7 +225,7 @@ export class MedicineSupplierService {
     if (!medicine) {
       throw new HttpException(
         this.medicineError.notFoundMedicine(),
-        HttpStatus.BAD_GATEWAY,
+        HttpStatus.NOT_FOUND,
       );
     }
     return {
